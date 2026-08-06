39fdb62972e75f4c37626f1bba279f51275dca95
Njabulo25 <njabulontsako49@gmail.com>
2026-08-06 22:30:09 +0200

    Remove debug files

--- fix_backtest.py
+++
@@ -1,8 +0,0 @@
-# fix_backtest.py
-code = open('backtest_combined.py', 'r').read()
-code = code.replace(
-    "scalper = LondonScalper(df_5m, sys.modules[__name__])",
-    "df_15m = pd.read_csv('data/raw/XAUUSD_15Min.csv', parse_dates=['time'])\ndf_15m = df_15m.sort_values('time').reset_index(drop=True)\nscalper = LondonScalper(df_15m, df_5m, sys.modules[__name__])"
-)
-open('backtest_combined.py', 'w').write(code)
-print("Backtest updated to pass 15-min data")
--- run_dashboard.py
+++
@@ -1,7 +0,0 @@
-# run_dashboard.py
-import sys
-from pathlib import Path
-sys.path.insert(0, str(Path(__file__).parent))
-from src.engine.web_dashboard import run_dashboard
-
-run_dashboard()
